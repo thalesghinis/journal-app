3385ec89a277112f591382a3805afbbd2f34992a
Caio Ghinis <caio.ghinis1@gmail.com>
2023-01-02 15:22:07 -0300

    Primeiros componentes

---
+++ pages/components/Layout.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+type ChildrenProps = {
+    onClick(): void;
+    name: string;
+    children: React.ReactNode;
+  };
+
+
+export default function Layout({children} : ChildrenProps){
+    return(
+        <>
+        <Head>
+            <title>Journal App</title>
+        </Head>
+         <Navbar/>
+         <main>{children}</main>         
+        </>
+    )
+}
